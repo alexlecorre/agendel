1ec4909570909a3068ea9a7961ef20c953bb7f3a
alex lecorre <alexlecorre@macbook-air-de-alex-2.home>
2025-03-07 13:41:43 +0100

    Mise à jour du schéma event.js

--- schemaTypes/event.js
+++ schemaTypes/event.js
@@ -1,15 +1,16 @@
 export default {
   name: 'event',
-  title: 'Événement',
+  title: 'evenement',
   type: 'document',
   fields: [
     {name: 'title', title: 'Titre', type: 'string'},
     {name: 'description', title: 'Description', type: 'text'},
-    {name: 'date', title: 'Date', type: 'datetime'},
+    {name: 'begindate', title: 'Date de debut', type: 'datetime'},
+    {name: 'enddate', title: 'Date de fin', type: 'datetime'},
     {name: 'price', title: 'Prix', type: 'string'},
     {name: 'image', title: 'Image', type: 'image'},
-    {name: 'category', title: 'Catégorie', type: 'string'},
-    {name: 'tags', title: 'Étiquettes', type: 'array', of: [{type: 'string'}]},
+    {name: 'category', title: 'Categorie', type: 'string'},
+    {name: 'tags', title: 'etiquettes', type: 'array', of: [{type: 'string'}]},
     {name: 'venue', title: 'Lieu', type: 'reference', to: [{type: 'venue'}]},
   ],
 }
